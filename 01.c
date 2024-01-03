c8a4ee3e1e4b8e757f1de17bf84f77da56a0352f
Abderrahman031 <155048377+Abderrahman031@users.noreply.github.com>
2024-01-03 18:27:12 +0200

    01.c

--- 01.c
+++ 01.c
@@ -1,8 +1,6 @@
 /******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+        Convert decimal to binary number system using bitwise operators
 
 *******************************************************************************/
 
